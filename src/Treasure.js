e37a3dbdf8436c09e805c71afc7ff71ca7a77fb3
Khrystyna Leshko <christinelechko@gmail.com>
2021-02-12 18:34:00 +0100

    done with Filipe help

--- src/Treasure.js
+++ src/Treasure.js
@@ -1,11 +1,15 @@
 class Treasure {
   constructor() {
-    this.col = 500;
-    this.row = 500;
+    this.col = this.setRandomPosition();
+    this.row = this.setRandomPosition();
+    this.width = WIDTH / 10;
+    this.height = HEIGHT / 10;
   }
   setRandomPosition() {
-    this.col = Math.floor(Math.random() * 1000);
-    this.row = Math.floor(Math.random() * 1000);
+    // this.col = Math.floor(Math.random() * 1000);
+    this.col = Math.floor(Math.random() * 10) * 100;
+    // this.row = Math.floor(Math.random() * 1000);
+    this.row = Math.floor(Math.random() * 10) * 100;
   }
   drawTreasure() {
     image(treasurePic, this.row, this.col, 90, 90);

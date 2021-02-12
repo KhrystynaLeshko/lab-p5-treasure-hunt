d2e334a697f4e1ff55ab34a10a913bc002fd2af2
Khrystyna Leshko <christinelechko@gmail.com>
2021-02-12 19:02:43 +0100

    collisionCheck doesn't work

--- src/game.js
+++ src/game.js
@@ -21,7 +21,45 @@ class Game {
   draw() {
     this.player.draw();
     this.treasure.drawTreasure();
+    if (this.collisionCheck(this.player, this.treasure)) {
+      noLoop();
+      const button = document.createElement("button");
+      button.innerText = "Yupi de poopi! You won! Play again?";
+      button.style.background = "green";
+      document.body.appendChild(button);
+
+      button.onclick = () => {
+        this.treasure.setRandomPosition();
+        button.parentNode.removeChild(button);
+        this.score++;
+        score.innerText = this.score;
+        loop();
+      };
+    }
   }
+  collisionCheck(player, treasure) {
+    const playerTopArea = player.y;
+    const playerLeftArea = player.x;
+    const playerRightArea = player.x + player.width;
+    const playerBottomArea = player.y + player.height;
+
+    const obsTopArea = treasure.y;
+    const obsLeftArea = treasure.x;
+    const obsRightArea = treasure.x + treasure.width;
+    const obsBottomArea = treasure.y + treasure.height;
+    const isTouchingOnLeft = obsRightArea > playerLeftArea;
+    const isTouchingOnBottom = obsTopArea < playerBottomArea;
+    const isTouchingOnRight = obsLeftArea < playerRightArea;
+    const isTouchingOnTop = obsBottomArea > playerTopArea;
+
+    return (
+      isTouchingOnRight &&
+      isTouchingOnTop &&
+      isTouchingOnBottom &&
+      isTouchingOnLeft
+    );
+  }
+
   keyPressed() {
     if (keyCode === 37) {
       game.player.moveLeft();
